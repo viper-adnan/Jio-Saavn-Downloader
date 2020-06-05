0303fd3ba9af77d1946c6ee3aab47d98e342d5d3
Parveen Bhadoo <bhadoo@pm.me>
2020-06-05 18:23:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -71,9 +71,9 @@ else {
 }
 function myFunction() {
   var x = document.getElementById("footer");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
