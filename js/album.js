196d2c3d3680f06ee5844d4d9296a1b817771c11
Parveen Bhadoo <bhadoo@pm.me>
2020-06-06 09:30:32 +0530

    Update album.js

--- js/album.js
+++ js/album.js
@@ -16,14 +16,14 @@ if (surl == null) {
     document.getElementById("status").innerHTML = "<h5>Welcome to<br>JioSaavn Downloader</h5><a href='/?url=https://www.jiosaavn.com/song/meri-aashiqui/RV4pdS5obh4'><img src='https://c.saavncdn.com/952/Meri-Aashiqui-Hindi-2020-20200602234001-500x500.jpg' width='250px' height='250px'>";
     document.getElementById("input").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' value='' autocomplete='off' required><input type='submit' value='Search'>";
 }
-else if (surl.startsWith("http://") || surl.startsWith("https://") || surl.startsWith("www.jiosaavn.com") || surl.startsWith("jiosaavn.com") || surl.startsWith("www.saavn.com") || surl.startsWith("saavn.com")) {
+else if (surl.startsWith("https://www.jiosaavn.com/album/")) {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
             var t = JSON.parse(this.responseText);
-            var e = t.url;
+            var e = t.perma_url;
             if (e == null) {
-            document.getElementById("status").innerHTML = "<h5>Please Enter JioSaavn Song Link</h5>" ;
+            document.getElementById("status").innerHTML = "<h5>Please Enter JioSaavn Album Link</h5>" ;
             document.getElementById("input").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' value='' autocomplete='off' required><input type='submit' value='Search'>";
                 if (surl != null){
                 document.getElementById("download").innerHTML = "Unable to Download from this Link" ;
@@ -34,11 +34,27 @@ else if (surl.startsWith("http://") || surl.startsWith("https://") || surl.start
     document.getElementById("input").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' value='' autocomplete='off' required><input type='submit' value='Search'>";
             }
         }
-        else if (e.endsWith(".mp3") === true){
-            var m = e.replace("http://h.saavncdn.com", "/play");
-            document.getElementById("input").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' value='" + t.title + " By " + t.singers + "' autocomplete='off' required><input type='submit' value='Search'>";
-            document.getElementById("download").innerHTML = "<table class='table table-striped'> <thead> <tr> <th scope='col'>Name</th> <td>" + t.title + "</td> </tr> </thead> <tbody> <tr> <th scope='row'>Singer</th> <td>" + t.singers + "</td> </tr> <tr> <th scope='row'>Album</th> <td>" + t.album + "</td> </tr> <tr> <th scope='row'>Language</th> <td>" + t.language + "</td> </tr> <tr> <th scope='row'>Label</th> <td>" + t.label + "</td> </tr> </tbody> </table><a href='" + t.url + "' class='button7' style='background-color:#2979FF'>Download MP3 320kbps</a>"
-            document.getElementById("status").innerHTML = "<img src='" + t.image_url + "' width='250px' height='250px'><br><br><center><audio controls style='width: 100%; max-width:600px;'> <source src='" + m + "' type='audio/ogg'> <source src='" + m + "' type='audio/mpeg'> Your browser does not support the audio element. </audio></center>" ;
+              
+              
+        else if (t.image.endsWith(".jpg") === true){
+            var obj, xmlhttp, myObj, x, txt = "";
+        var fsurl = surl.replace(/\+/g, " ");
+        obj = { table: "customers", limit: 20 };
+        dbParam = JSON.stringify(obj);
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            myObj = JSON.parse(this.responseText);
+            txt += "<table class='table table-striped table-bordered'><caption>Results for : " + fsurl + "</caption><thead><tr><th scope='col'>Name</th><th scope='col'>Link</th></tr></thead><tbody>"
+            for (x in myObj) {
+              txt += "<tr><td>" + myObj[x].songs.song + " By " + myObj[x].songs.singers + "</td><td><a href='/?url=" + myObj[x].songs.perma_url + "' target='_blank'>Open</a></td></tr>";
+            }
+            txt += "</tbody></table>"  
+            document.getElementById("input").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' value='" + fsurl + "' autocomplete='off' required><input type='submit' value='Search'>";
+            document.getElementById("download").innerHTML = txt;
+            document.getElementById("status").innerHTML = "" ;
+              
+              
         }
           }
         else {
