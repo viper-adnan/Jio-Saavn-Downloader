d31c733416bc51c100937f3adacdf52e92bcb967
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-09-20 18:35:48 +0530

    Experimental

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ if (surl == null) {
                 var m = newurl;
                 var dl = newurl.replace("https://", "/cdn.jiosaavn.ga/"); 
                 document.title = t.song + " By " + t.singers + " - JioSaavn.ga";
-                document.getElementById("input").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' onfocus='this.value=\"\"' value='" + t.song + " By " + t.singers + "' autocomplete='off' required><input type='submit' value='Search'>";
+                document.getElementById("links").innerHTML = "<input type='text' id='srcid' name='url' placeholder='Enter Song Name or JioSaavn Link' onfocus='this.value=\"\"' value='" + t.song + " By " + t.singers + "' autocomplete='off' required><input type='submit' value='Search'>";
                 document.getElementById("download").innerHTML = "<table class='table table-striped'> <thead> <tr> <th scope='col'>Name</th> <td>" + t.song + "</td> </tr> </thead> <tbody> <tr> <th scope='row'>Singer</th> <td>" + t.singers + "</td> </tr> <tr> <th scope='row'>Album</th> <td><a href='/album/?url=" + t.album_url + "'>" + t.album + "</a></td> </tr> <tr> <th scope='row'>Language</th> <td>" + t.language + "</td> </tr> <tr> <th scope='row'>Label</th> <td>" + t.label + "</td> </tr> </tbody> </table>"
                 document.getElementById("status").innerHTML = "<img src='" + t.image + "' width='250px' height='250px'><br><br><center><audio controls style='width: 100%; max-width:600px;' loop> <source src='" + m + "' type='audio/mp3'> <source src='" + dl + "' type='audio/mp4'> Your browser does not support the audio element. </audio></center>";
                 var status1 = "";
@@ -99,7 +99,7 @@ if (surl == null) {
                       headerMap1[header1] = value1;
                       var status1 = headerMap1["status"];
                       if (status1 != "403") {
-                      document.getElementById("links").innerHTML = "<a href='" + dl + "' class='button7' style='background-color:#2979FF' target='_self' download='" + t.song + " By " + t.singers + " From " + t.album + ".mp3'>Download Song</a>";
+                      document.getElementById("input").innerHTML = "<a href='" + dl + "' class='button7' style='background-color:#2979FF' target='_self' download='" + t.song + " By " + t.singers + " From " + t.album + ".mp3'>Download Song</a>";
                       }  
                       }
                     );
